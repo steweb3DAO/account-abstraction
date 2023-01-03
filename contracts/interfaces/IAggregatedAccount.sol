789d85c1496da8e565a56f8f35bc0f434ed0b3be
UFOVERSE <ufoverse@outlook.com>
2023-01-03 22:46:59 +0800

    read code

--- contracts/interfaces/IAggregatedAccount.sol
+++ contracts/interfaces/IAggregatedAccount.sol
@@ -8,10 +8,10 @@ import "./IAggregator.sol";
 /**
  * Aggregated account, that support IAggregator.
  * - the validateUserOp will be called only after the aggregator validated this account (with all other accounts of this aggregator).
+ * - 只有在聚合器验证该帐户(以及该聚合器的所有其他帐户)之后，才会调用 validateUserOp。
  * - the validateUserOp MUST valiate the aggregator parameter, and MAY ignore the userOp.signature field.
  */
 interface IAggregatedAccount is IAccount {
-
     /**
      * return the address of the signature aggregator the account supports.
      */
--- contracts/interfaces/IAggregator.sol
+++ contracts/interfaces/IAggregator.sol
@@ -5,32 +5,49 @@ import "./UserOperation.sol";
 
 /**
  * Aggregated Signatures validator.
+ * 一堆验签
  */
 interface IAggregator {
-
     /**
+     * 验签一堆用户操作
      * validate aggregated signature.
      * revert if the aggregated signature does not match the given list of operations.
+     * 验证聚合签名。
+     * 如果聚合签名与给定的操作列表不匹配，则恢复。是不是把 userOps 里面的内容直接用私钥做了签名？然后这里做验签
      */
-    function validateSignatures(UserOperation[] calldata userOps, bytes calldata signature) external view;
+    function validateSignatures(
+        UserOperation[] calldata userOps,
+        bytes calldata signature
+    ) external view;
 
     /**
+     * 验证一个用户操作
      * validate signature of a single userOp
      * This method is should be called by bundler after EntryPoint.simulateValidation() returns (reverts) with ValidationResultWithAggregation
      * First it validates the signature over the userOp. then it return data to be used when creating the handleOps:
      * @param userOp the userOperation received from the user.
      * @return sigForUserOp the value to put into the signature field of the userOp when calling handleOps.
      *    (usually empty, unless account and aggregator support some kind of "multisig"
+     * 验证单个 userOp 的签名
+     * 这个方法应该在 EntryPoint.simulateValidation() 和 ValidationResultWithAggregation 返回后由 Bundler 调用
+     * 首先验证 userOp 上的签名。然后返回创建 handleOps 时使用的数据
+     * @param userOp the userOperation received from the user.
+     * @return sigForUserOp 返回调用 handleOps 时放入 userOp 签名字段的值。
+     * (通常为空，除非帐户和聚合器支持某种“multisig”）
      */
-    function validateUserOpSignature(UserOperation calldata userOp)
-    external view returns (bytes memory sigForUserOp);
+    function validateUserOpSignature(
+        UserOperation calldata userOp
+    ) external view returns (bytes memory sigForUserOp);
 
     /**
+     * TODO off-chain 和 validateSignatures 区别是什么？
      * aggregate multiple signatures into a single value.
      * This method is called off-chain to calculate the signature to pass with handleOps()
      * bundler MAY use optimized custom code perform this aggregation
      * @param userOps array of UserOperations to collect the signatures from.
      * @return aggregatesSignature the aggregated signature
      */
-    function aggregateSignatures(UserOperation[] calldata userOps) external view returns (bytes memory aggregatesSignature);
+    function aggregateSignatures(
+        UserOperation[] calldata userOps
+    ) external view returns (bytes memory aggregatesSignature);
 }
